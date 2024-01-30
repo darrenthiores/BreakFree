fa83b42e1d15e5d86a060efbd0e9042fc867e493
darren thiores <darrenthiores@gmail.com>
2024-01-30 22:11:56 +0700

    Initial commit (inc buildSrc)

---
+++ buildSrc/src/main/java/Compose.kt
@@ -0,0 +1,24 @@
+object Compose {
+    const val composeVersion = "1.2.1"
+    const val composeCompilerVersion = "1.3.2"
+    const val material = "androidx.compose.material:material:$composeVersion"
+    const val icons ="androidx.compose.material:material-icons-extended:$composeVersion"
+    const val ui = "androidx.compose.ui:ui:$composeVersion"
+    const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:$composeVersion"
+    const val debugUiToolingPreview = "androidx.compose.ui:ui-tooling:$composeVersion"
+    const val debugUiTestManifest = "androidx.compose.ui:ui-test-manifest:$composeVersion"
+    const val runtime = "androidx.compose.runtime:runtime:$composeVersion"
+    const val compiler = "androidx.compose.compiler:compiler:$composeCompilerVersion"
+
+    private const val navigationVersion = "2.4.2"
+    const val navigation = "androidx.navigation:navigation-compose:$navigationVersion"
+
+    private const val hiltNavigationComposeVersion = "1.0.0"
+    const val hiltNavigationCompose = "androidx.hilt:hilt-navigation-compose:$hiltNavigationComposeVersion"
+
+    private const val activityComposeVersion = "1.3.1"
+    const val activityCompose = "androidx.activity:activity-compose:$activityComposeVersion"
+
+    private const val lifecycleVersion = "2.5.1"
+    const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion"
+}
---
+++ buildSrc/src/main/java/Oss.kt
@@ -0,0 +1,4 @@
+object Oss {
+    private const val ossVerison = "2.9.11"
+    const val oss = "com.aliyun.dpa:oss-android-sdk:$ossVerison"
+}
